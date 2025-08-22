41fb50b78c03194aab95f04b6bd09a5058d18e25
quoctichle <quoctichle@gmail.com>
2025-08-22 11:38:35 +0700

    Fix: chuyển file API động về đúng chuẩn Nuxt 3 (server/api/spin-codes/[code].get.js)

---
+++ server/api/spin-codes/[code].get.js
@@ -0,0 +1,19 @@
+import { connectDB } from '../mongodb'
+import mongoose from 'mongoose'
+
+const SpinCodeSchema = new mongoose.Schema({
+  code: { type: String, required: true, unique: true },
+  fb: String,
+  cid: String,
+  email: String,
+  prize: String,
+}, { timestamps: true })
+
+const SpinCode = mongoose.models.SpinCode || mongoose.model('SpinCode', SpinCodeSchema)
+
+export default defineEventHandler(async (event) => {
+  await connectDB()
+  const code = event.context.params.code
+  const doc = await SpinCode.findOne({ code })
+  return { success: !!doc, data: doc }
+})
